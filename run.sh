d8356b30737c63ef6f991dfef3b248d079cc899f
yfzhang <fdoctor00@gmail.com>
2017-12-29 17:08:16 +0800

    sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ git pull
 kill -9 $(ps -ef | grep "28018" | grep -v grep | awk '{print $2}')
 kill -9 $(ps -ef | grep "positiongo" | grep -v grep | awk '{print $2}')
 
-sudo mongod    --dbpath=/usr/local/var/mongodb  --port 28018
-go run main.go positiongo
-caddy
+sudo mongod    --dbpath=/usr/local/var/mongodb  --port 28018 &
+go run main.go positiongo &
+caddy &
